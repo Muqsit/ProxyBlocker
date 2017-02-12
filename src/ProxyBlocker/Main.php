a9bc6f9d96509575eb95ba745e1239e38851f30f
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-02-12 17:07:20 +0300

    Update Main.php

--- src/ProxyBlocker/Main.php
+++ src/ProxyBlocker/Main.php
@@ -1,12 +1,19 @@
 <?php
 namespace ProxyBlocker;
 
+use pocketmine\Player;
+
 class Main extends \pocketmine\plugin\PluginBase {
 
     private $server;
     public function onEnable()
     {
-        ($this->server = $this->getServer())->getPluginManager()->registerEvents(new EventListener, $this);
+        ($this->server = $this->getServer())->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->server->getLogger()->info("ProxyBlocker is now enabled.");
+    }
+    
+    public function checkForProxy(Player $player)
+    {
+        $this->server->getScheduler()->scheduleAsyncTask(new ProxyChecker([$player->getAddress(), $player->getName()]));
     }
 }
