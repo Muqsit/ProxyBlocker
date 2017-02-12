78ada3948281bc0404999e00847515402529febd
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-02-12 17:12:13 +0300

    Update Main.php

--- src/ProxyBlocker/Main.php
+++ src/ProxyBlocker/Main.php
@@ -5,15 +5,17 @@ use pocketmine\Player;
 
 class Main extends \pocketmine\plugin\PluginBase {
 
-    private $server;
+    private $server, $email;
     public function onEnable()
     {
         ($this->server = $this->getServer())->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->server->getLogger()->info("ProxyBlocker is now enabled.");
+        $random = "email".mt_rand(0, 10).mt_rand(0, 10).mt_rand(0, 10).mt_rand(0, 10)."@".["gmail.com", "yahoo.com", "hotmail.com"][mt_rand(0, 2)];
+        $this->email = yaml_parse_file($this->getDataFolder()."config.yml")["email"] ?? $random;
     }
     
     public function checkForProxy(Player $player)
     {
-        $this->server->getScheduler()->scheduleAsyncTask(new ProxyChecker([$player->getAddress(), $player->getName()]));
+        $this->server->getScheduler()->scheduleAsyncTask(new ProxyChecker([$player->getAddress(), $player->getName(), $this->email]));
     }
 }
