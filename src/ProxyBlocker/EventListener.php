87688f13833a90324737219fc023620e80308b88
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-04-12 22:35:46 +0300

    Update EventListener.php

--- src/ProxyBlocker/EventListener.php
+++ src/ProxyBlocker/EventListener.php
@@ -7,11 +7,14 @@ use pocketmine\event\player\PlayerLoginEvent;
 class EventListener implements Listener {
 
     private $plugin;
-    public function __construct(Main $plugin) {
+    
+    public function __construct(Main $plugin)
+    {
         $this->plugin = $plugin;
     }
 
-    public function onLogin(PlayerLoginEvent $event) {
+    public function onLogin(PlayerLoginEvent $event)
+    {
         $this->plugin->checkForProxy($event->getPlayer());
     }
 }
